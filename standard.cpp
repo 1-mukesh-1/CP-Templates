f3ffe7cde2d286a7ddc32cb56f83b56e289e668e
1-mukesh-1 <51417684+1-mukesh-1@users.noreply.github.com>
2021-04-16 19:58:31 +0530

    basic template C++

---
+++ standard.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve(){
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
