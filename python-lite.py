d2d4cd20b0e34beccedee6c8b2059f1dbd71e747
1-mukesh-1 <51417684+1-mukesh-1@users.noreply.github.com>
2021-07-30 19:56:51 +0530

    Create python-lite.py

---
+++ python-lite.py
@@ -0,0 +1,6 @@
+from math import (floor,ceil,factorial as fact,pow,log2,log10,log,pi as PI)
+def linput():
+    return list(map(int,input().split()))
+for _ in range(int(input())):
+    n=int(input())
+    l=linput()
